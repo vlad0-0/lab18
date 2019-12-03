8fefe176c0107d531e2f13cade4af742c2da1df4
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-12-03 13:38:54 +0300

    Update 18.1.py

--- 18.1.py
+++ 18.1.py
@@ -24,11 +24,11 @@ while x1!=x2 and y1!=y2:
         print(a[y][i])
     x = i
     y2 -= 1
-    for i in range(y2, y1-1):
+    for i in range(y2, y1-1, -1):
         print(a[i][x])
     y = i
     x2 -= 1
-    for i in range(x2, x1-1):
+    for i in range(x2, x1-1, -1):
         print(a[y][i])
     x = i
     y1 += 1
